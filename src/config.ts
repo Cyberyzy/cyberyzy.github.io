a090f75cd19006e5a36ae2587f51f28f3a0289a6
Eric Yang <110450075+Cyberyzy@users.noreply.github.com>
2024-09-23 18:29:18 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://cyberyzy.github.io", // replace this with your deployed domain
+  author: "Eric Yang",
+  profile: "https://cyberyzy.github.io",
+  desc: "A note of my life",
+  title: "Eric Yang's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
